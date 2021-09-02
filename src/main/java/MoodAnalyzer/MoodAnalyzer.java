57c3562483a90362927f8133450a7037d547c036
Nikhil Musale <nikhilmusale70@gmail.com>
2021-09-02 10:03:29 +0530

    [Nikhil] - added happy sad test cases

--- src/main/java/MoodAnalyzer/MoodAnalyzer.java
+++ src/main/java/MoodAnalyzer/MoodAnalyzer.java
@@ -1,4 +1,15 @@
 package MoodAnalyzer;
 
+import java.util.Locale;
+
 public class MoodAnalyzer {
+
+    public String analyseMood(String message){
+        if (message.toLowerCase().contains("happy"))
+            return "happy";
+        else if (message.toLowerCase().contains("sad"))
+            return "sad";
+        else
+            return "happy";
+    }
 }
--- src/test/java/MoodAnalyzerTest/MoodAnalyzerTest.java
+++ src/test/java/MoodAnalyzerTest/MoodAnalyzerTest.java
@@ -1,5 +1,15 @@
 package MoodAnalyzerTest;
 
+import MoodAnalyzer.MoodAnalyzer;
+import org.junit.Assert;
+import org.junit.Test;
+
 public class MoodAnalyzerTest {
-    
+    MoodAnalyzer moodAnalyzer = new MoodAnalyzer();
+
+    @Test
+    public void moodTest(){
+        String result = moodAnalyzer.analyseMood("I am happy");
+        Assert.assertEquals("happy", result);
+    }
 }
