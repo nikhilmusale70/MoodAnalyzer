2362f7b70d4acd3ad4d855cd067661a8e44dd5fb
Nikhil Musale <nikhilmusale70@gmail.com>
2021-09-02 10:22:53 +0530

    [Nikhil] - added constructor for taking message

--- src/main/java/MoodAnalyzer/MoodAnalyzer.java
+++ src/main/java/MoodAnalyzer/MoodAnalyzer.java
@@ -3,8 +3,17 @@ package MoodAnalyzer;
 import java.util.Locale;
 
 public class MoodAnalyzer {
+    public String message;
 
-    public String analyseMood(String message){
+    public MoodAnalyzer(String message) {
+        this.message = message;
+    }
+
+    public MoodAnalyzer(){
+
+    }
+
+    public String analyseMood(){
         if (message.toLowerCase().contains("happy"))
             return "happy";
         else if (message.toLowerCase().contains("sad"))
--- src/test/java/MoodAnalyzerTest/MoodAnalyzerTest.java
+++ src/test/java/MoodAnalyzerTest/MoodAnalyzerTest.java
@@ -5,11 +5,30 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class MoodAnalyzerTest {
-    MoodAnalyzer moodAnalyzer = new MoodAnalyzer();
+
+    @Test
+    public void moodHappy() {
+        MoodAnalyzer moodAnalyzer = new MoodAnalyzer("I am happy");
+        String result = moodAnalyzer.analyseMood();
+        Assert.assertEquals("happy", result);
+    }
 
     @Test
     public void moodTest(){
-        String result = moodAnalyzer.analyseMood("I am happy");
+
+    }
+
+    @Test
+    public void moodTestSad() {
+        MoodAnalyzer moodAnalyzer = new MoodAnalyzer("I am sad");
+        String result = moodAnalyzer.analyseMood();
+        Assert.assertEquals("sad", result);
+    }
+
+    @Test
+    public void MoodAnyIsHappy() {
+        MoodAnalyzer moodAnalyzer = new MoodAnalyzer("I am in any mood");
+        String result = moodAnalyzer.analyseMood();
         Assert.assertEquals("happy", result);
     }
 }
